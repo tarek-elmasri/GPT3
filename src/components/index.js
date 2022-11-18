2c6e1c5190567481944d7b9dc5e1d2d5d381fd9e
tarek-elmasry <dr.tareqelmasry@hotmail.com>
2022-11-19 02:51:50 +0300

    add Header section

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 export { default as Navbar } from './navbar/Navbar';
 export { default as Brand } from './brand/Brand';
+export { default as Feature } from './feature/Feature';
+
 
 
---
+++ src/containers/whatGPT3/WhatGPT3.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react"
+import WhatGPT3 from "./WhatGPT3"
+
+describe('WhatGPT3 Component', () => {
+  test('WhatGPT3 Snapshot', () => {
+    render(<WhatGPT3 />)
+    expect(screen).toMatchSnapshot()
+  })
+})
